5f22c6526ea4567c7e63351b3c171b8fa615c7c3
DJMcMayhem <jimmy.harrington@rocketmail.com>
2016-05-14 22:26:26 -0600

    Added two mappings: H=gg, and L=G$

--- rc.py
+++ rc.py
@@ -1,6 +1,8 @@
 n_maps = [
 "nnoremap {} :norm ".format(chr(238)),
-"nnoremap {} :%norm ".format(chr(206))
+"nnoremap {} :%norm ".format(chr(206)),
+"nnoremap H gg",
+"nnoremap l G$"
 ]
 
 def source(V):
