297e7382b8a75741d3b8267887d363fcb3348c1a
DJMcMayhem <jimmy.harrington@rocketmail.com>
2016-05-21 11:03:47 -0600

    Added regex file. It isn't called yet.

---
+++ regex.py
@@ -0,0 +1,15 @@
+#'s' is a string of bytes that will be exapnded into the regex that will be
+#sent to neovim. If the sequence is incomplete, it will return an empty string
+#to signify that we are still in the regex.
+def expand_regex(s):
+    if s[-1:] not in ['\r', '\n']:
+        return ""
+    expanded = ""
+    for c in s:
+        if ord(c) >= 128:
+            expanded += '\\' + chr(ord(c) - 128)
+        else:
+            expanded += c
+
+    return expanded
+
